ef1cffc52d79dac7fb046858f3930d1ef1e7462b
Тигран Погосян <136622719+TigranPoghos@users.noreply.github.com>
2023-10-18 19:45:58 +0300

    js

--- js/script.js
+++ js/script.js
@@ -24,3 +24,24 @@ document.addEventListener('click', function(event){
     closeModal(modal)
   }
 })
+
+
+// let person = {
+//   firstName:"Andrey",
+//   lastName:"Ivanov",
+//   age:20,
+//   isStudent:true,
+//   contactInfo: {
+//     "phone": "098-556-33-41",
+//     "email": "AndreyIvanov@gmail.com"
+//   },
+//   disciplines: [
+//     "Programming", "Machine engineering", "English"
+//   ]
+// }
+
+// let jsonObj = JSON.stringify(person)
+// console.log(jsonObj)
+
+// let personFromJson = JSON.parse(jsonObj)
+// console.log(personFromJson)
